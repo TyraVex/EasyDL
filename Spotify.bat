23a3b425fa4f39f9aa5dfefb798e6cefdab31c20
TyraVex <thomas.baruzier@gmail.com>
2022-11-06 20:54:54 +0100

    update

--- Spotify.bat
+++ Spotify.bat
@@ -3,7 +3,7 @@ cd %userprofile%\Downloads
 if not exist "Spotify musics" mkdir "Spotify musics"
 cd "Spotify musics"
 set /p link="Spotify link or song title : "
-spotdl "%link%" --st 20 --dt 10
+spotdl download "%link%"
 del .spotdl-cache
 explorer "..\Spotify musics"
 exit /b
