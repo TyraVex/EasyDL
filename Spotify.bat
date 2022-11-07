73a1bbdb5f434a5820bef6ff458006146ea42f37
TyraVex <thomas.baruzier@gmail.com>
2022-11-07 08:20:56 +0100

    update

--- Spotify.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd %userprofile%\Downloads
-if not exist "Spotify musics" mkdir "Spotify musics"
-cd "Spotify musics"
-set /p link="Spotify link or song title : "
-spotdl download "%link%"
-del .spotdl-cache
-explorer "..\Spotify musics"
-exit /b
--- Youtube Music.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd %userprofile%\Downloads
-if not exist "Youtube musics" mkdir "Youtube musics"
-cd "Youtube musics"
-set /p link="Youtube music link : "
-yt-dlp "%link%" --extractor-args youtube:player_client=android --throttled-rate 100K -x --audio-format mp3 --audio-quality 0 --embed-thumbnail --xattrs
-explorer "..\Youtube musics"
-exit /b
